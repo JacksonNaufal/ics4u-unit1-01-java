2e44e68a4f586e0d465b2c780deb7a09227f8f61
Jackson Naufal <Jackson.Naufal@mths.ca>
2022-09-14 20:59:45 +0000

    LogProgramFix

---
+++ LogProgram.java
@@ -0,0 +1,56 @@
+/*
+* The Log  program implements an application that
+* simply displays takes an input and displays the
+* amount of logs that can fit in a truck.
+*
+* @author  Jackson Naufal
+* @version 1.0
+* @since   2020-01-01
+*
+* This is the log calculator  program.
+*/
+
+import java.util.Scanner;
+
+/**
+ * This is a log calculating program.
+ */
+
+final class LogProgram {
+
+    /**
+    * This is the weight per log.
+    */
+    public static final double LBS = 20;
+
+    /**
+    * This is the max capacity in the truck.
+    */
+    public static final double CAP = 1100;
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private LogProgram() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+        // print out "Log Capacity!"
+        final Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter log length of logs!");
+        float LogProgram = myObj.nextFloat();
+        System.out.println("Number of log is:" + CAP / (LogProgram * LBS));
+        System.out.println("\nDone.");
+    }
+}
+
