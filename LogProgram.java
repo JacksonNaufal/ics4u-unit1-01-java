df96a912d43371c007d145c0e499ce393d2a5540
Jackson Naufal <Jackson.Naufal@mths.ca>
2022-09-14 23:42:58 +0000

    FixedLogProgram

--- LogProgram.java
+++ LogProgram.java
@@ -35,6 +35,7 @@ final class LogProgram {
     * @throws IllegalStateException
     *
     */
+
     private LogProgram() {
         throw new IllegalStateException("Cannot be instantiated");
     }
